b7c29110ff0a4871f6e59052be32f5a6bea8f4a7
Gagan <vishwakarmagagan24@gmail.com>
2024-06-08 17:15:31 +0530

    task18

---
+++ Project_Task_18/messageMixer.js
@@ -0,0 +1,65 @@
+function countCharacter(inputString, inputCharacter) {
+    let count = 0;
+    let string = inputString.toLowerCase();
+    let character = inputCharacter.toLowerCase();
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === character) {
+            count++;
+        }
+    }
+    return count;
+};
+
+function capitalizeFirstCharacterOfWords(string) {
+    let arr = string.split(" ");
+    for (let i = 0; i < arr.length; i++) {
+        let word = arr[i];
+        arr[i] = word[0].toUpperCase() + word.substring(1);
+    }
+    return arr.join(" ");
+};
+
+
+function reverseWord(word) {
+    return word.split("").reverse().join("");
+};
+
+function reverseAllWords(sentence) {
+    let words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = reverseWord(words[i]);
+    }
+    return words.join(" ");
+};
+
+
+function replaceFirstOccurence(string, toBeReplaced, replaceWith) {
+    return string.replace(toBeReplaced, replaceWith);
+};
+
+
+function replaceAllOccurrences(string, toBeReplaced, replaceWith) {
+    return string.split(toBeReplaced).join(replaceWith);
+};
+
+function encode(string) {
+    let replacementObject = { "a": "@", "s": "$", "i": "!", "o": "0" };
+    for (let key in replacementObject) {
+        string = replaceAllOccurrences(string, key, replacementObject[key]);
+    }
+    return string;
+};
+
+function palindrome(str) {
+    let check = str;
+    let reverse = reverseWord(check);
+    if (check === reverse){
+        return true;
+    };
+        return false
+}
+function pigLatin(sentence , character){
+ return  sentence.split(" ").join(character+" ");
+ 
+}
+export { encode, replaceAllOccurrences, palindrome ,pigLatin ,replaceFirstOccurence, reverseAllWords, reverseWord, capitalizeFirstCharacterOfWords, countCharacter }
